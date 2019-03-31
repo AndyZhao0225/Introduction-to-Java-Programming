e9d70ffb67e111026603490b86473006f3de2569
Junan Zhao <1499563136@qq.com>
2019-03-31 12:04:53 -0400

    Create Main.java

---
+++ GUI/Chap34/34-8/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+  public class Main 
+{
+     public static void main(String[] args)
+   {
+       JFrame frame = new MyFrame();
+       frame.setTitle("34-8");
+       frame.setSize(500,400);
+       frame.setLocationRelativeTo(null);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setVisible(true);
+   }    
+}
