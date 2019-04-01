40f04a1ce84f3f190eebde8521cce4fb73efd196
Junan Zhao <1499563136@qq.com>
2019-04-01 17:39:11 -0400

    Add files via upload

---
+++ Data-Structure/Chap26/26-1/TestBinaryTree.java
@@ -0,0 +1,45 @@
+public class TestBinaryTree {
+  public static void main(String[] args) {
+    // Create a BinaryTree
+    Integer[] numbers = {60,55,100,45,57,59,67,107,101};
+    BinaryTree<Integer> tree = new BinaryTree<Integer>(numbers);
+    
+    
+//    tree.insert("George");
+//    tree.insert("Michael");
+//    tree.insert("Tom");
+//    tree.insert("Adam");
+//    tree.insert("Jones");
+//    tree.insert("Peter");
+//    tree.insert("Daniel");
+
+    // Traverse tree
+    System.out.print("Inorder (sorted): \n");
+    tree.inorder();
+    System.out.print("\nPostorder: \n");
+    tree.postorder();
+    System.out.print("\nPreorder: \n");
+    tree.preorder();
+    System.out.print("\nBreadthFirstTraversal(): \n");
+    tree.breadthFirstTraversal();
+    
+    System.out.print("\nThe number of nodes is " + tree.getSize() + "\n");
+    System.out.print("\nThe height of the tree is " + tree.height() + "\n");
+
+//    // Search for an element
+//    System.out.print("\nIs Peter in the tree? " + 
+//      tree.search("Peter"));
+
+    // Get a path from the root to Peter
+//    System.out.print("\nA path from the root to Peter is: ");
+//    java.util.ArrayList<BinaryTree.TreeNode<String>>  path 
+//      = tree.path("Peter");
+//    for (int i = 0; path != null && i < path.size(); i++)
+//      System.out.print(path.get(i).element + " ");
+//
+//    Integer[] numbers = {2, 4, 3, 1, 8, 5, 6, 7};
+//    BinaryTree<Integer> intTree = new BinaryTree<Integer>(numbers);
+//    System.out.print("\nInorder (sorted): ");
+//    intTree.inorder();
+  }
+}
