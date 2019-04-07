ab579df162d0f110b7485a23feabfdff0de93fda
Junan Zhao <1499563136@qq.com>
2019-04-07 08:21:28 -0400

    Add files via upload

---
+++ Advanced Java/Chap30/30-1Server/Server.java
@@ -0,0 +1,66 @@
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.InetAddress;
+import java.util.Date;
+import java.text.DecimalFormat;    
+
+  public class Server extends JFrame
+{
+    private JTextArea area = new JTextArea();   
+    private DecimalFormat df = new DecimalFormat("#.00");
+    
+    public Server()
+  {
+      setLayout(new BorderLayout());
+      add(new JScrollPane(area),BorderLayout.CENTER);
+      
+      setTitle("Server");
+      setSize(500,300);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
+      
+      try
+        {
+           ServerSocket serverSocket = new ServerSocket(8000);  
+           area.append("Server started at " + new Date() + '\n');
+           Socket socket = serverSocket.accept();
+           InetAddress inetAddress= socket.getInetAddress();
+           area.append("Client's host name is " + inetAddress.getHostName() + "\n");
+           area.append("Client's IP address is " + inetAddress.getHostAddress() + "\n");
+           
+           DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+           DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+           
+           while(true)
+             {
+                double annualInterestRate = inputFromClient.readDouble();
+                int numberOfYears = inputFromClient.readInt();
+                double loanAmount = inputFromClient.readDouble();
+                
+                double monthlyInterestRate = annualInterestRate/12;
+                double monthlyPayment = loanAmount*monthlyInterestRate/(1 - 1/Math.pow(1 + monthlyInterestRate,numberOfYears*12));
+                double totalPayment = monthlyPayment*12*numberOfYears;
+                
+                outputToClient.writeDouble(monthlyPayment);
+                outputToClient.writeDouble(totalPayment);
+                
+                area.append("Annual Interest Rate: " + annualInterestRate + "  ");
+                area.append("Number of Years: " + numberOfYears + "  ");
+                area.append("Loan Amount: " + loanAmount + "\n");
+                area.append("Monthly Payment: " + df.format(monthlyPayment) + "  ");
+                area.append("Total Payment: " + df.format(totalPayment) + "\n\n");
+             }
+        }
+      catch(IOException ex)
+        {
+           System.err.println(ex);
+        }
+  }
+}
