9633dc593671b031817cde8e907ebf829da648c3
Junan Zhao <1499563136@qq.com>
2019-04-12 09:49:23 -0400

    Add files via upload

---
+++ Data-Structure/Chap24/24-8/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+  public class Main 
+{
+    public static void main(String[] args)
+  {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Assign a size to the array: ");
+      int n = input.nextInt();
+      Integer[] list = new Integer[n];
+      System.out.println("Enter the array: ");
+      for( int i=0; i<=n-1; i++)
+          list[i] = input.nextInt();
+      sort(list);
+      for( int i=0; i<=n-1; i++)
+          System.out.print(list[i] + " ");
+      System.out.println();
+  }   
+    
+    public static <E extends Comparable<E>> void sort(E[] list)
+  {
+      Heap<E> heap = new Heap<E>();
+      for( int i=0; i<=list.length-1; i++)
+          heap.add(list[i]);
+      for( int i=list.length-1; i>=0; i--)
+          list[i] = heap.remove();
+  }
+}
