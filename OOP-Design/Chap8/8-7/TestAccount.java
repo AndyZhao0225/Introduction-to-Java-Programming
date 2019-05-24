e6e9914ef6b293e7f21ab4a819c8c49f984fffd0
Junan Zhao <1499563136@qq.com>
2019-05-23 21:17:02 -0400

    Add files via upload

---
+++ OOP-Design/Chap8/8-7/TestAccount.java
@@ -0,0 +1,84 @@
+import java.util.Date;
+
+  public class TestAccount 
+{
+    public static void main(String[] args)
+  {
+      Account account = new Account(1122,20000);
+      account.setAnnualInterestRate(0.045);
+      account.withdraw(2500);
+      account.deposit(3000);
+      System.out.println("Account Balance: "+account.getBalance());
+      System.out.println("Monthly Interest Rate: "+account.getMonthlyInterestRate()*100+"%");
+      System.out.println("The Date the Account was opened: "+account.getDateCreated().toString());
+  }    
+}
+
+  class Account 
+{
+    private int id = 0;
+    private double balance = 0;
+    private static double annualInterestRate = 0;
+    private Date dateCreated = new Date();
+    
+    Account()
+  {
+  
+  }
+    
+    Account(int newId, int newBalance)
+  {
+      id = newId;
+      balance = newBalance;
+  }
+    
+    public int getId()
+  {
+      return id;
+  }
+    
+    public double getBalance()
+  {
+      return balance;
+  }
+    
+    public static double getAnnualInterestRate()
+  {
+      return annualInterestRate;
+  }
+    
+    public void setId(int newId)
+  {
+      id = newId;
+  }
+    
+    public void setBalance(double newBalance)
+  {
+      balance = (newBalance>0)? newBalance: 0;
+  }
+    
+    public void setAnnualInterestRate(double newAnnualInterestRate)
+  {
+      annualInterestRate = (newAnnualInterestRate>0)? newAnnualInterestRate: 0;
+  }
+    
+    public Date getDateCreated()
+  {
+      return dateCreated;
+  }
+    
+    public double getMonthlyInterestRate()
+  {
+      return annualInterestRate/12;
+  }
+    
+    public void withdraw(double amount)
+  {
+      balance -= amount;
+  }
+    
+    public void deposit(double amount)
+  {
+      balance += amount;
+  }
+}
