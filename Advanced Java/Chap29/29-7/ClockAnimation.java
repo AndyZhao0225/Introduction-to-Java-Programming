17a83fc806980a068eff73f6713ffbbc47350321
Junan Zhao <1499563136@qq.com>
2019-05-29 23:01:57 -0400

    Update ClockAnimation.java

--- Advanced Java/Chap29/29-7/ClockAnimation.java
+++ Advanced Java/Chap29/29-7/ClockAnimation.java
@@ -3,7 +3,6 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.Timer;
 
 /**
  * @author  Junan Zhao
