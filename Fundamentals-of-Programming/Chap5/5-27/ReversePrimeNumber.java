a265e7a48760854e1669b90d41cbd3dc1065137b
Junan Zhao <1499563136@qq.com>
2019-06-02 08:45:03 -0400

    Add files via upload

---
+++ Fundamentals-of-Programming/Chap5/5-27/ReversePrimeNumber.java
@@ -0,0 +1,50 @@
+  public class ReversePrimeNumber 
+{
+    public static void main(String[] args)
+  {
+      final int AMOUNT_OF_NUMBERS = 100; 
+      final int NUMBERS_PER_LINE = 10;
+      int count=0;
+      int number=2;
+      while(count < AMOUNT_OF_NUMBERS)
+           {
+              if( isPrime(number) && ! isPalindrome(number) && isPrime(reverse(number)) ) 
+                {
+                   System.out.printf("%6d    ",number);
+                   count++;
+                   if( count%10==0 ) System.out.println();
+                }
+              number++;
+           } 
+  }    
+    
+    public static int reverse(int number)
+  {
+      int reverse=0,remaining=number,i=0;
+      while(remaining!=0)
+          {
+             remaining= remaining/10;
+             i++;
+          }
+      remaining=number;
+      for(int j=0; j<i; j++)
+         {
+             int digit= remaining%10;
+             remaining= remaining/10;
+             reverse= reverse+digit*(int)Math.pow(10,i-1-j);
+         }
+      return reverse;
+  }
+    
+    public static boolean isPalindrome(int number)
+  {
+      return reverse(number)==number; 
+  }
+    
+    public static boolean isPrime(int number)
+  {
+      for( int divisor=2; divisor<=number/2; divisor++)
+         if(number%divisor==0) return false;
+      return true;    
+  }
+}
