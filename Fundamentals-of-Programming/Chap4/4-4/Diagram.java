3d7a73ae88259e889f2b1454da227ec3d2a11630
Junan Zhao <1499563136@qq.com>
2019-06-04 07:53:01 -0400

    Add files via upload

---
+++ Fundamentals-of-Programming/Chap4/4-4/Diagram.java
@@ -0,0 +1,8 @@
+  public class Diagram 
+{
+    public static void main(String[] args)
+  {
+      System.out.println("mile              kilometer");
+      for(int i=1;i<=10;i++)System.out.printf("%-2d                 %-5.3f\n",i,i*1.609);
+  }    
+}
