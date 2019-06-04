88df3b48147a877118ae14e56e5d0216c7bf0e3b
Junan Zhao <1499563136@qq.com>
2019-06-04 07:52:26 -0400

    Add files via upload

---
+++ Fundamentals-of-Programming/Chap4/4-3/Diagram.java
@@ -0,0 +1,8 @@
+  public class Diagram 
+{
+    public static void main(String[] args)
+  {
+      System.out.println("Kilogram           Pound"); 
+      for(int i=1;i<=199;i=i+2)System.out.printf("%-3d                 %-4.1f\n",i,i*2.2);  
+  } 
+}
