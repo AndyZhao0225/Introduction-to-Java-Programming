a1ca489a2a2ec0fda000a0f4e0d978147455655f
Junan Zhao <1499563136@qq.com>
2019-06-05 07:35:33 -0400

    Add files via upload

---
+++ Fundamentals-of-Programming/Chap4/4-12/NewClass.java
@@ -0,0 +1,9 @@
+  public class NewClass 
+{
+     public static void main(String[] args)
+   {
+       int n=1;
+       while(n*n<=12000)n++;
+       System.out.println("The smallest number among numbers whose squares are larger than 12000 is "+n);
+   }    
+}
