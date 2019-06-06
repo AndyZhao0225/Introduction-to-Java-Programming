fc2e0eb744a6f6954e441b6e29836433028270ed
Junan Zhao <1499563136@qq.com>
2019-06-06 18:03:58 -0400

    Add files via upload

---
+++ Fundamentals-of-Programming/Chap4/4-24/ComputingSumOfArray.java
@@ -0,0 +1,9 @@
+  public class ComputingSumOfArray 
+{
+    public static void main(String[] args) 
+  {
+      double sum=0,i;
+      for(i=1;i<=97;i+=2)sum=sum+i/(i+2);
+      System.out.println(sum);
+  }   
+}
