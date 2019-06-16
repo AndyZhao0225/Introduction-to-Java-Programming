5f69ea392aa6e3358490c206aa145fcbfbfa2386
Junan Zhao <1499563136@qq.com>
2019-06-16 09:36:55 -0400

    Add files via upload

---
+++ Fundamentals-of-Programming/Chap1/1-7/Pie.java
@@ -0,0 +1,8 @@
+  public class Pie
+{
+     public static void main(String[] args)
+   {
+     System.out.println("Pie=");
+     System.out.println(4*(1.0-1.0/3+1.0/5-1.0/7+1.0/9-1.0/11+1.0/13-1.0/15+1.0/17-1.0/19+1.0/21-1.0/23+1.0/25-1.0/27+1.0/29-1.0/31+1.0/33-1.0/35));
+   }
+}
